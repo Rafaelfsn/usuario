2897a4fd511f374c512c3e380f3fd29f6e7b986f
Rafael Neto <rafaelneto.fs@gmail.com>
2026-02-27 20:41:29 -0300

    adicionado método salvaUsuario

---
+++ src/main/java/com/javanauta/usuario/business/UsuarioService.java
@@ -0,0 +1,24 @@
+package com.javanauta.usuario.business;
+
+import com.javanauta.usuario.business.converter.UsuarioConverter;
+import com.javanauta.usuario.business.dto.UsuarioDTO;
+import com.javanauta.usuario.infrastructure.entity.Usuario;
+import com.javanauta.usuario.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioConverter usuarioConverter;
+
+    public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+        Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO); //RECEBE USUARIO DTO E CONVERTE PARA ENTITY
+        usuario = usuarioRepository.save(usuario); //SALVA USUARIO ENTITY
+        return usuarioConverter.paraUsuarioDTO(usuario); //CONVERTE USUARIO ENTITY PARA DTO E RETORNA USUARIO DTO
+    }
+
+
+}
